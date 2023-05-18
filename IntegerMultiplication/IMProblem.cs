6a92e511614f68fd406aea87a8779d3af869e403
Admin <Admin@SHROUK>
2023-05-18 23:32:26 +0300

    Add project files.

---
+++ IntegerMultiplication/IMProblem.cs
@@ -0,0 +1,236 @@
+﻿using Helpers;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Problem
+{
+
+    public class Problem : ProblemBase, IProblem
+    {
+        #region ProblemBase Methods
+        public override string ProblemName { get { return "IntegerMultiplication"; } }
+
+        public override void TryMyCode()
+        {
+            int N = 0;
+            byte[] output;
+
+            N = 2;
+            byte[] X1 = { 1, 1 };
+            byte[] Y1 = { 2, 2 };
+            byte[] expected = { 2, 4, 2, 0 };
+            output = IntegerMultiplication.IntegerMultiply(X1, Y1, N);
+            PrintCase(N, X1, Y1, output, expected);
+
+            N = 4;
+            byte[] X2 = { 1, 2, 3, 4 }; 
+            byte[] Y2 = { 0, 0, 0, 0 };
+            byte[] expected2 = { 0, 0, 0, 0, 0, 0, 0, 0 };
+            output = IntegerMultiplication.IntegerMultiply(X2, Y2, N);
+            PrintCase(N, X2, Y2, output, expected2);
+
+            N = 4;
+            byte[] X21 = { 9, 9, 9, 9 };
+            byte[] Y21 = { 9, 9, 9, 9 };
+            byte[] expected21 = { 1, 0, 0, 0, 8, 9, 9, 9 };
+            output = IntegerMultiplication.IntegerMultiply(X21, Y21, N);
+            PrintCase(N, X21, Y21, output, expected21);
+
+            N = 8;
+            byte[] X3 = { 0, 0, 0, 0, 1, 2, 3, 4 };
+            byte[] Y3 = { 0, 0, 0, 0, 1, 0, 0, 0 };
+            byte[] expected3 = { 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 0, 0, 0, 0};
+            output = IntegerMultiplication.IntegerMultiply(X3, Y3, N);
+            PrintCase(N, X3, Y3, output, expected3);
+
+            N = 8;
+            byte[] X4 = { 2, 7, 0, 1, 3, 1, 0, 0 };
+            byte[] Y4 = { 2, 7, 0, 1, 3, 1, 0, 0 };
+            byte[] expected4 = { 4, 8, 1, 9, 6, 8, 9, 7, 1, 7, 1, 0, 0, 0, 0, 0 };
+            output = IntegerMultiplication.IntegerMultiply(X4, Y4, N);
+            PrintCase(N, X4, Y4, output, expected4);
+
+            N = 16;
+            byte[] X5 = { 4, 8, 1, 9, 6, 8, 9, 7, 1, 7, 1, 0, 0, 0, 0, 0 };
+            byte[] Y5 = { 4, 8, 1, 9, 6, 8, 9, 7, 1, 7, 1, 0, 0, 0, 0, 0 };
+            byte[] e5 = { 6, 5, 8, 5, 2, 8, 2, 5, 3, 9, 7, 1, 5, 0, 9, 7, 4, 1, 5, 9, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            output = IntegerMultiplication.IntegerMultiply(X5, Y5, N);
+            PrintCase(N, X5, Y5, output, e5);
+        }
+
+ 
+        Thread tstCaseThr;
+        bool caseTimedOut ;
+        bool caseException;
+
+        protected override void RunOnSpecificFile(string fileName, HardniessLevel level, int timeOutInMillisec)
+        {
+            int testCases;
+            int N = 0;
+            byte[] output = null;
+            byte[] actualResult = null;
+            int j=0;
+
+            Stream s = new FileStream(fileName, FileMode.Open);
+            BinaryReader br = new BinaryReader(s);
+
+            testCases = br.ReadInt32();
+
+            int totalCases = testCases;
+            int correctCases = 0;
+            int wrongCases = 0;
+            int timeLimitCases = 0;
+
+            int i = 1;
+            while (testCases-- > 0)
+            {
+                N = br.ReadInt32();
+                byte[] X = new byte[N];
+                byte[] Y = new byte[N];
+                actualResult = new byte[2*N];
+
+                for (j = 0; j < N; j++)
+                {
+                    X[j] = br.ReadByte();
+                }
+                for (j = 0; j < N; j++)
+                {
+                    Y[j] = br.ReadByte();
+                }
+                for (j = 0; j < 2*N; j++)
+                {
+                    actualResult[j] = br.ReadByte();
+                }
+                caseTimedOut = true;
+                caseException = false;
+                {
+                    tstCaseThr = new Thread(() =>
+                    {
+                        try
+                        {
+                            Stopwatch sw = Stopwatch.StartNew();
+                            output = IntegerMultiplication.IntegerMultiply(X, Y, N);
+                            sw.Stop();
+                            Console.WriteLine("N = {0}, time in ms = {1}", N, sw.ElapsedMilliseconds);
+                        }
+                        catch
+                        {
+                            caseException = true;
+                            output = null;
+                        }
+                        caseTimedOut = false;
+                    });
+
+                    //StartTimer(timeOutInMillisec);
+                    tstCaseThr.Start();
+                    tstCaseThr.Join(timeOutInMillisec);
+                }
+                                
+                if (caseTimedOut)       //Timedout
+                {
+                    Console.WriteLine("Time Limit Exceeded in Case {0}.", i);
+                    tstCaseThr.Abort();
+                    timeLimitCases++;
+                }
+                else if (caseException) //Exception 
+                {
+                    Console.WriteLine("Exception in Case {0}.", i);
+                    wrongCases++;
+                }
+                else if (output != null && CheckOutput(output, actualResult))    //Passed
+                {
+                    Console.WriteLine("Test Case {0} Passed!", i);
+                    correctCases++;
+                }
+                else                    //WrongAnswer
+                {
+                    Console.WriteLine("Wrong Answer in Case {0}.", i);
+                    if (level == HardniessLevel.Easy)
+                    {
+                        if (output != null)
+                        {
+                            PrintCase(N, X, Y, output, actualResult);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Exception is occur");
+                        }
+                    }
+                    wrongCases++;
+                }
+
+                i++;
+            }
+            s.Close();
+            br.Close();
+            Console.WriteLine();
+            Console.WriteLine("# correct = {0}", correctCases);
+            Console.WriteLine("# time limit = {0}", timeLimitCases);
+            Console.WriteLine("# wrong = {0}", wrongCases);
+            Console.WriteLine("\nFINAL EVALUATION (%) = {0}", Math.Round((float)correctCases / totalCases * 100, 0)); 
+        }
+
+       
+
+        protected override void OnTimeOut(DateTime signalTime)
+        {
+        }
+
+        public override void GenerateTestCases(HardniessLevel level, int numOfCases)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Helper Methods
+        private static void PrintNum(byte[] X)
+        {
+
+            int N = X.Length;
+
+            for (int i = N - 1; i >= 0; i--)
+            {
+                    Console.Write(X[i]);
+            }
+            Console.WriteLine();
+        }
+
+        
+        private void PrintCase(int N, byte[] X, byte[] Y, byte[] output, byte[] expected)
+        {
+            Console.WriteLine("N = {0}", N);
+            Console.Write("X = "); PrintNum(X);
+            Console.Write("Y = "); PrintNum(Y);
+            Console.Write("output = "); if (output != null) PrintNum(output); else Console.WriteLine("NULL");
+            Console.Write("expect = "); PrintNum(expected);
+            if (output != null)
+            {
+                if (CheckOutput(output, expected)) Console.WriteLine("CORRECT");
+                else Console.WriteLine("WRONG");
+            }
+            Console.WriteLine();
+        }
+
+        private bool CheckOutput(byte[] output, byte[] actualResult)
+        {
+            int N = output.Length;
+            for (int idx = 0; idx < N; idx++)
+            {
+                if (output[idx] != actualResult[idx]) return false;
+            }
+            
+            return true;
+        }
+        #endregion
+   
+    }
+}
---
+++ IntegerMultiplication/IntegerMultiplication.cs
@@ -0,0 +1,386 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem
+{
+    // *****************************************
+    // DON'T CHANGE CLASS OR FUNCTION NAME
+    // YOU CAN ADD FUNCTIONS IF YOU NEED TO
+    // *****************************************
+    public static class IntegerMultiplication
+    {
+        #region YOUR CODE IS HERE
+
+        //Your Code is Here:
+        //==================
+        /// <summary>
+        /// Multiply 2 large integers of N digits in an efficient way [Karatsuba's Method]
+        /// </summary>
+        /// <param name="X">First large integer of N digits [0: least significant digit, N-1: most signif. dig.]</param>
+        /// <param name="Y">Second large integer of N digits [0: least significant digit, N-1: most signif. dig.]</param>
+        /// <param name="N">Number of digits (power of 2)</param>
+        /// <returns>Resulting large integer of 2xN digits (left padded with 0's if necessarily) [0: least signif., 2xN-1: most signif.]</returns>
+
+        //Subtraction
+        public static byte[] subtraction(byte[] a, byte[] b)
+        {
+
+            int max_length = Math.Max(a.Length, b.Length);
+            int min_length = Math.Min(a.Length, b.Length);
+            byte[] result = new byte[max_length];
+            for (int i = 0; i < max_length; i++)
+            {
+
+                if (i < min_length)
+                {
+                    if (b[i] > a[i])
+                    {
+                        for (int j = i + 1; j < a.Length; j++)
+                        {
+                            if (a[j] == 0)
+                            {
+                                a[j] += 9;
+                            }
+                            else
+                            {
+                                a[i] = (byte)(a[i] + 10);
+                                a[j] = (byte)(a[j] - 1);
+                                result[i] = (byte)(a[i] - b[i]);
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        result[i] = (byte)(a[i] - b[i]);
+
+                    }
+                }
+                else
+                {
+                    if (max_length == a.Length)
+                    {
+                        result[i] = a[i];
+
+                    }
+                    else
+                    {
+                        result[i] = b[i];
+                    }
+                }
+
+            }
+            return result;
+        }
+        //Additio
+        /* public static byte[] Addtion(byte[] a, byte[] b)
+         {
+             int max_length = Math.Max(a.Length, b.Length);
+             int min_length = Math.Min(a.Length, b.Length);
+             List<byte> L = new List<byte>(max_length);
+             int carry = 0, sum;
+             for (int i = 0; i < max_length; i++)
+             {
+                 if (min_length != max_length)
+                 {
+                     if (i < min_length)
+                     {
+                         sum = a[i] + b[i] + carry;
+                         L.Add((byte)(sum % 10));
+                         carry = sum / 10;
+                     }
+                     else
+                     {
+
+                         if (max_length == a.Length)
+                         {
+                             if (carry != 0)
+                             {
+                                 sum = a[i] + carry;
+                                 L.Add((byte)(sum % 10));
+                                 carry = sum / 10;
+                             }
+                             else
+                             {
+                                 L.Add((byte)a[i]);
+                             }
+                             if (i == a.Length - 1)
+                             {
+                                 if (carry != 0)
+                                 {
+                                     L.Add((byte)(carry % 10));
+                                     break;
+                                 }
+                             }
+                         }
+                         else
+                         {
+                             if (carry != 0)
+                             {
+                                 sum = b[i] + carry;
+                                 L.Add((byte)(sum % 10));
+                                 carry = sum / 10;
+                             }
+                             else
+                             {
+                                 L.Add((byte)b[i]);
+                             }
+                             if (i == b.Length - 1)
+                             {
+                                 if (carry != 0)
+                                 {
+                                     L.Add((byte)(carry % 10));
+                                     break;
+                                 }
+                             }
+                         }
+                     }
+
+                 }
+                 else
+                 {
+                     sum = a[i] + b[i] + carry;
+                     L.Add((byte)(sum % 10));
+                     carry = sum / 10;
+                     if (i == a.Length - 1)
+                     {
+                         if (carry != 0)
+                         {
+                             L.Add((byte)(carry % 10));
+                         }
+                     }
+                 }
+
+             }
+             byte[] ab = L.ToArray();
+             return ab;
+
+         }*/
+        /*public static byte[] Addtion(byte[] a, byte[] b)
+        {
+            int max_length = Math.Max(a.Length, b.Length);
+            int min_length = Math.Min(a.Length, b.Length);
+            if (max_length != min_length)
+            {
+                if (a.Length == max_length)
+                {
+                    b = Padding(b, Math.Abs(a.Length - b.Length));
+                }
+                else
+                {
+                    a = Padding(a, Math.Abs(a.Length - b.Length));
+                }
+            }
+            List<byte> L = new List<byte>(max_length);
+            int carry = 0, sum;
+            for (int i = 0; i < max_length; i++)
+            {
+                sum = a[i] + b[i] + carry;
+                L.Add((byte)(sum % 10));
+                carry = sum / 10;
+                if (i == a.Length - 1)
+                {
+                    if (carry != 0)
+                    {
+                        L.Add((byte)(carry % 10));
+                    }
+                }
+            }
+            byte[] ab = L.ToArray();
+            return ab;
+        }*/
+        public static byte[] Addtion(byte[] a, byte[] b)
+        {
+            int max_length = Math.Max(a.Length, b.Length);
+            int min_length = Math.Min(a.Length, b.Length);
+            List<byte> L = new List<byte>(max_length);
+            int carry = 0, sum;
+            if (a.Length > b.Length)
+            {
+                for (int i = 0; i < b.Length; i++)
+                {
+                    sum = a[i] + b[i] + carry;
+                    L.Add((byte)(sum % 10));
+                    carry = sum / 10;
+                }
+                for (int i = b.Length; i < a.Length; i++)
+                {
+                    if (carry != 0)
+                    {
+                        sum = a[i] + carry;
+                        L.Add((byte)(sum % 10));
+                        carry = sum / 10;
+                    }
+                    else
+                    {
+                        L.Add((byte)a[i]);
+                    }
+                    if (i == a.Length - 1)
+                    {
+                        if (carry != 0)
+                        {
+                            L.Add((byte)(carry % 10));
+                            break;
+                        }
+                    }
+                }
+            }
+            else if (b.Length > a.Length)
+            {
+                for (int i = 0; i < a.Length; i++)
+                {
+                    sum = a[i] + b[i] + carry;
+                    L.Add((byte)(sum % 10));
+                    carry = sum / 10;
+                }
+                for (int i = a.Length; i < b.Length; i++)
+                {
+                    if (carry != 0)
+                    {
+                        sum = b[i] + carry;
+                        L.Add((byte)(sum % 10));
+                        carry = sum / 10;
+                    }
+                    else
+                    {
+                        L.Add((byte)a[i]);
+                    }
+                    if (i == b.Length - 1)
+                    {
+                        if (carry != 0)
+                        {
+                            L.Add((byte)(carry % 10));
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < max_length; i++)
+                {
+                    sum = a[i] + b[i] + carry;
+                    L.Add((byte)(sum % 10));
+                    carry = sum / 10;
+                    if (i == a.Length - 1)
+                    {
+                        if (carry != 0)
+                        {
+                            L.Add((byte)(carry % 10));
+                        }
+                    }
+                }
+            }
+            byte[] ab = L.ToArray();
+            return ab;
+
+        }
+        //Padding
+        static public byte[] Padding(byte[] arr, int n)
+        {
+            byte[] result = new byte[arr.Length + n];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                result[i] = arr[i];
+            }
+
+            return result;
+        }
+        //Ten_Power
+        public static byte[] Ten_Power(byte[] arr, int n, int N)
+        {
+            byte[] result = new byte[2 * N];
+            for (int i = 0; i < arr.Length && (i + n) < 2 * N; i++)
+            {
+                result[i + n] = arr[i];
+            }
+
+            return result;
+        }
+        static public byte[] IntegerMultiply(byte[] X, byte[] Y, int N)
+        {
+            //REMOVE THIS LINE BEFORE START CODING
+            //throw new NotImplementedException();
+
+            //Base case
+            /*            if (N == 1)
+                        {
+                            byte[] Result = new byte[2];
+                            Result[0] = (byte)((X[0] * Y[0]) % 10);
+                            Result[1] = (byte)((X[0] * Y[0]) / 10);
+                            return Result;
+                        }*/
+            if (N <= 64)
+            {
+                byte[] product = new byte[2 * N];
+                int P = 0;
+                int carry = 0;
+                for (int i = 0; i < X.Length; i++)
+                {
+                    for (int j = 0; j < Y.Length; j++)
+                    {
+                        P = X[i] * Y[j] + product[i + j] + carry;
+                        carry = P / 10;
+                        P = P % 10;
+                        product[i + j] = (byte)P;
+
+                    }
+                    if (carry != 0)
+                    {
+                        product[i + Y.Length] += (byte)carry;
+                        carry = 0;
+                    }
+
+                }
+                return product;
+            }
+
+            //equals
+            if (X.Length > Y.Length)
+            {
+                Y = Padding(Y, (X.Length - Y.Length));
+            }
+            else
+            {
+                X = Padding(X, (Y.Length - X.Length));
+
+            }
+            // N must be even
+            if (N % 2 != 0)
+            {
+                N += 1;
+            }
+
+            //Split X & Y into halves: A & B ,C & D.
+            int mid_x = (N) / 2;
+            int mid_y = (N) / 2;
+            byte[] A = X.Take(mid_x).ToArray();
+            byte[] B = X.Skip(mid_x).ToArray();
+            byte[] C = Y.Take(mid_y).ToArray();
+            byte[] D = Y.Skip(mid_y).ToArray();
+
+            //conqure
+            byte[] AB = Addtion(A, B);
+            byte[] CD = Addtion(C, D);
+            int max_size = Math.Max(CD.Length, AB.Length);
+            byte[] Z = IntegerMultiply(AB, CD, max_size);
+            byte[] M1 = IntegerMultiply(A, C, A.Length);//"AC"
+            byte[] M2 = IntegerMultiply(B, D, B.Length);//"BD"
+            byte[] First_sub = subtraction(Z, M2);
+            byte[] BC_AD = subtraction(First_sub, M1);
+            M2 = Ten_Power(M2, N, N);
+            BC_AD = Ten_Power(BC_AD, N / 2, N);
+            byte[] M2_BC_AD = Addtion(M2, BC_AD);
+            byte[] RESULT = Addtion(M2_BC_AD, M1);
+            return RESULT;
+        }
+
+        #endregion
+    }
+   
+}
